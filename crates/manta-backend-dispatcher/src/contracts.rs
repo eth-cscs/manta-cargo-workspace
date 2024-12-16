8d78a627eac607d0653fcc503d98689bad08786c
Manuel Sopena Ballesteros <masber@hotmail.com>
2024-12-16 15:41:45 +0100

    fix: linter issues

--- crates/manta-backend-dispatcher/src/contracts.rs
+++ crates/manta-backend-dispatcher/src/contracts.rs
@@ -13,7 +13,7 @@ pub trait BackendTrait {
     // AUTHORIZATION
     fn get_api_token(
         &self,
-        site_name: &str,
+        _site_name: &str,
     ) -> impl std::future::Future<Output = Result<String, Error>> + Send {
         async {
             Err(Error::Message(
@@ -25,7 +25,7 @@ pub trait BackendTrait {
     // AUTHORIZATION : HSM/GROUP
     fn get_hsm_name_available(
         &self,
-        jwt_token: &str,
+        _jwt_token: &str,
     ) -> impl std::future::Future<Output = Result<Vec<String>, Error>> + Send {
         async {
             Err(Error::Message(
@@ -38,8 +38,8 @@ pub trait BackendTrait {
     // FIXME: rename function to 'get_hsm_group_members'
     fn get_member_vec_from_hsm_name_vec(
         &self,
-        auth_token: &str,
-        hsm_group_name_vec: Vec<String>,
+        _auth_token: &str,
+        _hsm_group_name_vec: Vec<String>,
     ) -> impl std::future::Future<Output = Result<Vec<String>, Error>> + Send {
         async {
             Err(Error::Message(
@@ -51,8 +51,8 @@ pub trait BackendTrait {
     // HSM/GROUP
     fn get_hsm_map_and_filter_by_hsm_name_vec(
         &self,
-        auth_token: &str,
-        hsm_name_vec: Vec<&str>,
+        _auth_token: &str,
+        _hsm_name_vec: Vec<&str>,
     ) -> impl std::future::Future<Output = Result<HashMap<String, Vec<String>>, Error>> + Send {
         async {
             Err(Error::Message(
@@ -65,7 +65,7 @@ pub trait BackendTrait {
     // HSM/GROUP
     fn get_all_hsm(
         &self,
-        auth_token: &str,
+        _auth_token: &str,
     ) -> impl std::future::Future<Output = Result<Vec<HsmGroup>, Error>> + Send {
         async {
             Err(Error::Message(
@@ -78,8 +78,8 @@ pub trait BackendTrait {
     // FIXME: Create a new type PowerStatus and return Result<PowerStatus, Error>
     fn power_on_sync(
         &self,
-        auth_token: &str,
-        nodes: &[String],
+        _auth_token: &str,
+        _nodes: &[String],
     ) -> impl std::future::Future<Output = Result<Value, Error>> + Send {
         async {
             Err(Error::Message(
@@ -92,9 +92,9 @@ pub trait BackendTrait {
     // FIXME: Create a new type PowerStatus and return Result<PowerStatus, Error>
     fn power_off_sync(
         &self,
-        auth_token: &str,
-        nodes: &[String],
-        force: bool,
+        _auth_token: &str,
+        _nodes: &[String],
+        _force: bool,
     ) -> impl std::future::Future<Output = Result<Value, Error>> + Send {
         async {
             Err(Error::Message(
@@ -107,9 +107,9 @@ pub trait BackendTrait {
     // FIXME: Create a new type PowerStatus and return Result<PowerStatus, Error>
     fn power_reset_sync(
         &self,
-        auth_token: &str,
-        nodes: &[String],
-        force: bool,
+        _auth_token: &str,
+        _nodes: &[String],
+        _force: bool,
     ) -> impl std::future::Future<Output = Result<Value, Error>> + Send {
         async {
             Err(Error::Message(
@@ -121,8 +121,8 @@ pub trait BackendTrait {
     // BSS/BOOTPARAMETERS
     fn get_bootparameters(
         &self,
-        auth_token: &str,
-        nodes: &[String],
+        _auth_token: &str,
+        _nodes: &[String],
     ) -> impl std::future::Future<Output = Result<Vec<BootParameters>, Error>> + Send {
         async {
             Err(Error::Message(
@@ -134,8 +134,8 @@ pub trait BackendTrait {
     // BSS/BOOTPARAMETERS
     fn update_bootparameters(
         &self,
-        auth_token: &str,
-        boot_parameters: &BootParameters,
+        _auth_token: &str,
+        _boot_parameters: &BootParameters,
     ) -> impl std::future::Future<Output = Result<(), Error>> + Send {
         async {
             Err(Error::Message(
@@ -145,7 +145,7 @@ pub trait BackendTrait {
     }
 
     // BSS/BOOTPARAMETERS
-    fn add_kernel_params(&mut self, new_kernel_params: &str) -> Result<bool, Error> {
+    fn add_kernel_params(&mut self, _new_kernel_params: &str) -> Result<bool, Error> {
         Err(Error::Message(
             "Add kernel parameters command not implemented for this backend".to_string(),
         ))

afd471cce734bdafdddd2c86226155af283d9daf
Manuel Sopena Ballesteros <masber@hotmail.com>
2024-12-16 15:32:35 +0100

    fix: desugar async trait methods

--- crates/manta-backend-dispatcher/src/contracts.rs
+++ crates/manta-backend-dispatcher/src/contracts.rs
@@ -11,104 +11,137 @@ pub trait BackendTrait {
     fn test_backend_trait(&self) -> String;
 
     // AUTHORIZATION
-    async fn get_api_token(&self, site_name: &str) -> Result<String, Error> {
-        Err(Error::Message(
-            "Authentication command not implemented for this backend".to_string(),
-        ))
+    fn get_api_token(
+        &self,
+        site_name: &str,
+    ) -> impl std::future::Future<Output = Result<String, Error>> + Send {
+        async {
+            Err(Error::Message(
+                "Authentication command not implemented for this backend".to_string(),
+            ))
+        }
     }
 
-    // HSM/GROUP
-    async fn get_hsm_name_available(&self, jwt_token: &str) -> Result<Vec<String>, Error> {
-        Err(Error::Message(
-            "Get HSM name available command not implemented for this backend".to_string(),
-        ))
+    // AUTHORIZATION : HSM/GROUP
+    fn get_hsm_name_available(
+        &self,
+        jwt_token: &str,
+    ) -> impl std::future::Future<Output = Result<Vec<String>, Error>> + Send {
+        async {
+            Err(Error::Message(
+                "Get HSM name available command not implemented for this backend".to_string(),
+            ))
+        }
     }
 
     // HSM/GROUP
     // FIXME: rename function to 'get_hsm_group_members'
-    async fn get_member_vec_from_hsm_name_vec(
+    fn get_member_vec_from_hsm_name_vec(
         &self,
         auth_token: &str,
         hsm_group_name_vec: Vec<String>,
-    ) -> Result<Vec<String>, Error> {
-        Err(Error::Message(
-            "Get group members command not implemented for this backend".to_string(),
-        ))
+    ) -> impl std::future::Future<Output = Result<Vec<String>, Error>> + Send {
+        async {
+            Err(Error::Message(
+                "Get group members command not implemented for this backend".to_string(),
+            ))
+        }
     }
 
     // HSM/GROUP
-    async fn get_hsm_map_and_filter_by_hsm_name_vec(
+    fn get_hsm_map_and_filter_by_hsm_name_vec(
         &self,
         auth_token: &str,
         hsm_name_vec: Vec<&str>,
-    ) -> Result<HashMap<String, Vec<String>>, Error> {
-        Err(Error::Message(
-            "Get HSM map and filter by HSM name command not implemented for this backend"
-                .to_string(),
-        ))
+    ) -> impl std::future::Future<Output = Result<HashMap<String, Vec<String>>, Error>> + Send {
+        async {
+            Err(Error::Message(
+                "Get HSM map and filter by HSM name command not implemented for this backend"
+                    .to_string(),
+            ))
+        }
     }
 
     // HSM/GROUP
-    async fn get_all_hsm(&self, auth_token: &str) -> Result<Vec<HsmGroup>, Error> {
-        Err(Error::Message(
-            "Get all HSM command not implemented for this backend".to_string(),
-        ))
+    fn get_all_hsm(
+        &self,
+        auth_token: &str,
+    ) -> impl std::future::Future<Output = Result<Vec<HsmGroup>, Error>> + Send {
+        async {
+            Err(Error::Message(
+                "Get all HSM command not implemented for this backend".to_string(),
+            ))
+        }
     }
 
     // PCS
     // FIXME: Create a new type PowerStatus and return Result<PowerStatus, Error>
-    async fn power_on_sync(&self, auth_token: &str, nodes: &[String]) -> Result<Value, Error> {
-        Err(Error::Message(
-            "Power on command not implemented for this backend".to_string(),
-        ))
+    fn power_on_sync(
+        &self,
+        auth_token: &str,
+        nodes: &[String],
+    ) -> impl std::future::Future<Output = Result<Value, Error>> + Send {
+        async {
+            Err(Error::Message(
+                "Power on command not implemented for this backend".to_string(),
+            ))
+        }
     }
 
     // PCS
     // FIXME: Create a new type PowerStatus and return Result<PowerStatus, Error>
-    async fn power_off_sync(
+    fn power_off_sync(
         &self,
         auth_token: &str,
         nodes: &[String],
         force: bool,
-    ) -> Result<Value, Error> {
-        Err(Error::Message(
-            "Power off command not implemented for this backend".to_string(),
-        ))
+    ) -> impl std::future::Future<Output = Result<Value, Error>> + Send {
+        async {
+            Err(Error::Message(
+                "Power off command not implemented for this backend".to_string(),
+            ))
+        }
     }
 
     // PCS
     // FIXME: Create a new type PowerStatus and return Result<PowerStatus, Error>
-    async fn power_reset_sync(
+    fn power_reset_sync(
         &self,
         auth_token: &str,
         nodes: &[String],
         force: bool,
-    ) -> Result<Value, Error> {
-        Err(Error::Message(
-            "Power reset command not implemented for this backend".to_string(),
-        ))
+    ) -> impl std::future::Future<Output = Result<Value, Error>> + Send {
+        async {
+            Err(Error::Message(
+                "Power reset command not implemented for this backend".to_string(),
+            ))
+        }
     }
 
     // BSS/BOOTPARAMETERS
-    async fn get_bootparameters(
+    fn get_bootparameters(
         &self,
         auth_token: &str,
         nodes: &[String],
-    ) -> Result<Vec<BootParameters>, Error> {
-        Err(Error::Message(
-            "Get boot parameters command not implemented for this backend".to_string(),
-        ))
+    ) -> impl std::future::Future<Output = Result<Vec<BootParameters>, Error>> + Send {
+        async {
+            Err(Error::Message(
+                "Get boot parameters command not implemented for this backend".to_string(),
+            ))
+        }
     }
 
     // BSS/BOOTPARAMETERS
-    async fn update_bootparameters(
+    fn update_bootparameters(
         &self,
         auth_token: &str,
         boot_parameters: &BootParameters,
-    ) -> Result<(), Error> {
-        Err(Error::Message(
-            "Update boot parameters command not implemented for this backend".to_string(),
-        ))
+    ) -> impl std::future::Future<Output = Result<(), Error>> + Send {
+        async {
+            Err(Error::Message(
+                "Update boot parameters command not implemented for this backend".to_string(),
+            ))
+        }
     }
 
     // BSS/BOOTPARAMETERS

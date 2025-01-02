3ae23463665e79c9a23259d6cba8484ebc6f12cb
Manuel Sopena Ballesteros <masber@hotmail.com>
2025-01-02 04:47:06 +0100

    feat: add constructor for HSM group struct

--- crates/manta-backend-dispatcher/src/types.rs
+++ crates/manta-backend-dispatcher/src/types.rs
@@ -5,27 +5,6 @@ use serde_json::Value;
 
 use crate::error::Error;
 
-#[derive(Debug, Serialize, Deserialize, Default, Clone)]
-pub struct BootParameters {
-    #[serde(default)]
-    pub hosts: Vec<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub macs: Option<Vec<String>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub nids: Option<Vec<u32>>,
-    #[serde(default)]
-    pub params: String, // FIXME: change type to HashMap<String, String> by using function
-    // bss::utils::convert_kernel_params_to_map AND create new method
-    // bss::BootParameters::num_kernel_params which returns the list of kernel parameters
-    #[serde(default)]
-    pub kernel: String,
-    #[serde(default)]
-    pub initrd: String,
-    #[serde(rename = "cloud-init")]
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub cloud_init: Option<Value>,
-}
-
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct HsmGroup {
     pub label: String,
@@ -49,6 +28,49 @@ pub struct Member {
 pub struct XnameId {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<String>,
+}
+
+impl HsmGroup {
+    pub fn new(label: &str, member_vec_opt: Option<Vec<&str>>) -> Self {
+        let members_opt = if let Some(member_vec) = member_vec_opt {
+            Some(Member {
+                ids: Some(member_vec.iter().map(|&id| id.to_string()).collect()),
+            })
+        } else {
+            None
+        };
+
+        let group = Self {
+            label: label.to_string(),
+            description: None,
+            tags: None,
+            members: members_opt,
+            exclusive_group: None,
+        };
+
+        group
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Default, Clone)]
+pub struct BootParameters {
+    #[serde(default)]
+    pub hosts: Vec<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub macs: Option<Vec<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub nids: Option<Vec<u32>>,
+    #[serde(default)]
+    pub params: String, // FIXME: change type to HashMap<String, String> by using function
+    // bss::utils::convert_kernel_params_to_map AND create new method
+    // bss::BootParameters::num_kernel_params which returns the list of kernel parameters
+    #[serde(default)]
+    pub kernel: String,
+    #[serde(default)]
+    pub initrd: String,
+    #[serde(rename = "cloud-init")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub cloud_init: Option<Value>,
 }
 
 impl BootParameters {

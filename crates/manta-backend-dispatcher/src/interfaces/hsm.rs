4362206b935bb3038098729eb282e3e9d377fd84
Manuel Sopena Ballesteros <masber@hotmail.com>
2025-01-26 11:43:15 +0100

    feat: testing segregating traits into modules by type of product

---
+++ crates/manta-backend-dispatcher/src/interfaces/hsm.rs
@@ -0,0 +1,34 @@
+use crate::{error::Error, types::ComponentArray};
+
+pub trait Component {
+    fn get_all_nodes(
+        &self,
+        auth_token: &str,
+        nid_only: Option<&str>,
+    ) -> impl std::future::Future<Output = Result<ComponentArray, Error>> + Send;
+
+    fn get(
+        &self,
+        auth_token: &str,
+        id: Option<&str>,
+        r#type: Option<&str>,
+        state: Option<&str>,
+        flag: Option<&str>,
+        role: Option<&str>,
+        subrole: Option<&str>,
+        enabled: Option<&str>,
+        software_status: Option<&str>,
+        subtype: Option<&str>,
+        arch: Option<&str>,
+        class: Option<&str>,
+        nid: Option<&str>,
+        nid_start: Option<&str>,
+        nid_end: Option<&str>,
+        partition: Option<&str>,
+        group: Option<&str>,
+        state_only: Option<&str>,
+        flag_only: Option<&str>,
+        role_only: Option<&str>,
+        nid_only: Option<&str>,
+    ) -> impl std::future::Future<Output = Result<ComponentArray, Error>> + Send;
+}

51bef9a755f678cc4b4006dd999e6cdb1c3bd34b
Manuel Sopena Ballesteros <masber@hotmail.com>
2025-02-16 04:27:32 +0100

    feat: migrate commands apply session and get configuration to backend dispatcher

--- crates/manta-backend-dispatcher/src/interfaces/cfs.rs
+++ crates/manta-backend-dispatcher/src/interfaces/cfs.rs
@@ -1,6 +1,9 @@
 use std::future::Future;
 
-use crate::types::K8sDetails;
+use crate::types::cfs::cfs_configuration_request::CfsConfigurationRequest;
+use crate::types::cfs::{CfsConfigurationResponse, Layer, LayerDetails};
+use crate::types::ims::Image;
+use crate::types::{BosSessionTemplate, K8sDetails};
 use crate::{error::Error, types::cfs::CfsSessionGetResponse};
 use futures::AsyncBufRead;
 
@@ -67,4 +70,82 @@ pub trait CfsTrait {
         k8s_api_url: &str,
         k8s: &K8sDetails,
     ) -> impl Future<Output = Result<Self::T, Error>> + Send;
+
+    fn get_configuration(
+        &self,
+        auth_token: &str,
+        base_url: &str,
+        root_cert: &[u8],
+        configuration_name_opt: Option<&String>,
+    ) -> impl Future<Output = Result<Vec<CfsConfigurationResponse>, Error>> + Send;
+
+    fn get_and_filter_configuration(
+        &self,
+        shasta_token: &str,
+        shasta_base_url: &str,
+        shasta_root_cert: &[u8],
+        configuration_name: Option<&str>,
+        configuration_name_pattern: Option<&str>,
+        hsm_group_name_vec: &[String],
+        limit_number_opt: Option<&u8>,
+    ) -> impl Future<Output = Result<Vec<CfsConfigurationResponse>, Error>> + Send;
+
+    fn get_configuration_layer_details(
+        &self,
+        shasta_root_cert: &[u8],
+        gitea_base_url: &str,
+        gitea_token: &str,
+        layer: Layer,
+    ) -> impl Future<Output = Result<LayerDetails, Error>> + Send;
+
+    fn create_configuration_from_repos(
+        &self,
+        gitea_token: &str,
+        gitea_base_url: &str,
+        shasta_root_cert: &[u8],
+        // repos: Vec<PathBuf>,
+        repo_name_vec: Vec<String>,
+        local_git_commit_vec: Vec<String>,
+        playbook_file_name_opt: Option<&String>,
+    ) -> impl Future<Output = Result<CfsConfigurationRequest, Error>>;
+
+    // This function enforces a new CFS configuration to be created. First, checks if CFS configuration
+    // with same name already exists in CSM, if that is the case, it will return an error, otherwise
+    // creates a new CFS configuration
+    fn put_configuration(
+        &self,
+        shasta_token: &str,
+        shasta_base_url: &str,
+        shasta_root_cert: &[u8],
+        configuration: &CfsConfigurationRequest,
+        configuration_name: &str,
+    ) -> impl Future<Output = Result<CfsConfigurationResponse, Error>> + Send;
+
+    fn update_runtime_configuration(
+        &self,
+        shasta_token: &str,
+        shasta_base_url: &str,
+        shasta_root_cert: &[u8],
+        xnames: Vec<String>,
+        desired_configuration: &str,
+        enabled: bool,
+    ) -> impl Future<Output = Result<(), Error>> + Send;
+
+    // Get all CFS sessions, IMS images and BOS sessiontemplates related to a CFS configuration
+    fn get_derivatives(
+        &self,
+        shasta_token: &str,
+        shasta_base_url: &str,
+        shasta_root_cert: &[u8],
+        configuration_name: &str,
+    ) -> impl Future<
+        Output = Result<
+            (
+                Option<Vec<CfsSessionGetResponse>>,
+                Option<Vec<BosSessionTemplate>>,
+                Option<Vec<Image>>,
+            ),
+            Error,
+        >,
+    > + Send;
 }
--- crates/manta-backend-dispatcher/src/types/mod.rs
+++ crates/manta-backend-dispatcher/src/types/mod.rs
@@ -1,4 +1,5 @@
 pub mod cfs;
+pub mod ims;
 
 use std::{collections::HashMap, str::FromStr};
 

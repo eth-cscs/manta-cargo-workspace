626b4d9252a352c85ac750f9c3c9d9763ead283e
Manuel Sopena Ballesteros <masber@hotmail.com>
2025-02-25 03:34:33 +0100

    feat: new functionalities add/update/delete boot parameters

--- crates/manta-backend-dispatcher/src/interfaces/bss.rs
+++ crates/manta-backend-dispatcher/src/interfaces/bss.rs
@@ -5,13 +5,25 @@ use crate::{error::Error, types::BootParameters};
 pub trait BootParametersTrait {
     fn get_bootparameters(
         &self,
-        _auth_token: &str,
-        _nodes: &[String],
+        auth_token: &str,
+        nodes: &[String], // FIXME: This argument should be a struct of type BootParameters
     ) -> impl Future<Output = Result<Vec<BootParameters>, Error>> + Send;
+
+    fn add_bootparameters(
+        &self,
+        auth_token: &str,
+        boot_parameters: &BootParameters,
+    ) -> impl Future<Output = Result<(), Error>> + Send;
 
     fn update_bootparameters(
         &self,
-        _auth_token: &str,
-        _boot_parameters: &BootParameters,
+        auth_token: &str,
+        boot_parameters: &BootParameters,
     ) -> impl Future<Output = Result<(), Error>> + Send;
+
+    fn delete_bootparameters(
+        &self,
+        auth_token: &str,
+        boot_parameters: &BootParameters,
+    ) -> impl Future<Output = Result<String, Error>> + Send;
 }

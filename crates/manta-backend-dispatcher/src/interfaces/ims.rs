e0ebb6bfec18edb5c305e1b647ce968a0d42c8d4
Manuel Sopena Ballesteros <masber@hotmail.com>
2025-04-14 16:01:04 +0200

    feat: add function 'delete_image' to ImsTrait

--- crates/manta-backend-dispatcher/src/interfaces/ims.rs
+++ crates/manta-backend-dispatcher/src/interfaces/ims.rs
@@ -16,4 +16,18 @@ pub trait ImsTrait {
             ))
         }
     }
+
+    fn delete_image(
+        &self,
+        _shasta_token: &str,
+        _shasta_base_url: &str,
+        _shasta_root_cert: &[u8],
+        _image_id: &str,
+    ) -> impl Future<Output = Result<(), Error>> + Send {
+        async {
+            Err(Error::Message(
+                "Delete image command not implemented for this backend".to_string(),
+            ))
+        }
+    }
 }

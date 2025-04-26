9cdc874dad4502e4f91b9e37141224f335d3323f
Manuel Sopena Ballesteros <masber@hotmail.com>
2025-04-26 15:13:00 +0200

    feat: Avoid external libraries from having to import kube-rs when connecting to node console

--- crates/csm-rs/src/backend_connector.rs
+++ crates/csm-rs/src/backend_connector.rs
@@ -11,6 +11,7 @@ use backend_dispatcher::{
         bss::BootParametersTrait,
         cfs::CfsTrait,
         commands::CommandsTrait,
+        console::ConsoleTrait,
         get_images_and_details::GetImagesAndDetailsTrait,
         hsm::{
             component::ComponentTrait, group::GroupTrait, hardware_inventory::HardwareInventory,
@@ -40,14 +41,17 @@ use backend_dispatcher::{
 use chrono::NaiveDateTime;
 use futures::{AsyncBufRead, AsyncReadExt};
 use hostlist_parser::parse;
+use kube::api::AttachedProcess;
 use regex::Regex;
 use serde_json::Value;
+use tokio::io::{AsyncRead, AsyncWrite};
 
 use crate::{
     bos,
     bss::{self},
     common::{authentication, kubernetes, vault::http_client::fetch_shasta_k8s_secrets_from_vault},
     hsm::{self, component::types::ComponentArrayPostArray, group::types::Member},
+    node::console,
     pcs,
 };
 
@@ -893,8 +897,6 @@ impl CfsTrait for Csm {
         // k8s_api_url: &str,
         k8s: &K8sDetails,
     ) -> Result<Pin<Box<dyn AsyncBufRead + Send>>, Error> {
-        // FIXME: this only takes the stream from the CFS session and not from the
-        // git-clone init container
         let shasta_k8s_secrets = match &k8s.authentication {
             K8sAuth::Native {
                 certificate_authority_data,
@@ -1906,5 +1908,53 @@ impl CommandsTrait for Csm {
         )
         .await
         .map_err(|e| Error::Message(e.to_string()))
+    }
+}
+
+impl ConsoleTrait for Csm {
+    type T = Box<dyn AsyncWrite + Unpin>;
+    type U = Box<dyn AsyncRead + Unpin>;
+
+    async fn attach_to_console(
+        &self,
+        shasta_token: &str,
+        site_name: &str,
+        xname: &str,
+        k8s: &K8sDetails,
+    ) -> Result<(Box<dyn AsyncWrite + Unpin>, Box<dyn AsyncRead + Unpin>), Error> {
+        let shasta_k8s_secrets = match &k8s.authentication {
+            K8sAuth::Native {
+                certificate_authority_data,
+                client_certificate_data,
+                client_key_data,
+            } => {
+                serde_json::json!({ "certificate-authority-data": certificate_authority_data, "client-certificate-data": client_certificate_data, "client-key-data": client_key_data })
+            }
+            K8sAuth::Vault {
+                base_url,
+                // secret_path: _secret_path,
+            } => fetch_shasta_k8s_secrets_from_vault(&base_url, shasta_token, &site_name)
+                .await
+                .map_err(|e| Error::Message(e.to_string()))?,
+        };
+
+        let mut attached: AttachedProcess = console::get_container_attachment_to_conman(
+            &xname.to_string(),
+            &k8s.api_url,
+            shasta_k8s_secrets,
+        )
+        .await
+        .map_err(|e| Error::Message(e.to_string()))?;
+
+        println!("Connected to {}!", xname,);
+        println!("Use &. key combination to exit the console.",);
+
+        println!("Connected to {}!", xname,);
+        println!("Use &. key combination to exit the console.",);
+
+        Ok((
+            Box::new(attached.stdin().unwrap()),
+            Box::new(attached.stdout().unwrap()),
+        ))
     }
 }

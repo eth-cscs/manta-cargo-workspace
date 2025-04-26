74926020de749d950e1c7c7a39a30cfb5ca7d878
Manuel Sopena Ballesteros <masber@hotmail.com>
2025-04-26 14:39:33 +0200

    feat: add funcionality to connect to node console

---
+++ crates/manta-backend-dispatcher/src/interfaces/console.rs
@@ -0,0 +1,25 @@
+use std::future::Future;
+
+use tokio::io::{AsyncRead, AsyncWrite};
+
+use crate::{error::Error, types::K8sDetails};
+
+/// This trait returns a tuple like `(stdin, stdout, stderr)`
+pub trait ConsoleTrait {
+    type T: AsyncWrite + Unpin;
+    type U: AsyncRead + Unpin;
+
+    fn attach_to_console(
+        &self,
+        _shasta_token: &str,
+        _site_name: &str,
+        _xname: &str,
+        _k8s: &K8sDetails,
+    ) -> impl Future<Output = Result<(Self::T, Self::U), Error>> + Send {
+        async {
+            Err(Error::Message(
+                "Attach to console command not implemented for this backend".to_string(),
+            ))
+        }
+    }
+}

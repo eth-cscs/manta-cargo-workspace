44dda7508e0af91eb35a4495fd554d2ba414c1df
Manuel Sopena Ballesteros <masber@hotmail.com>
2025-05-10 19:10:20 +0200

    feat: add interface to get all boot parameters

--- crates/manta-backend-dispatcher/src/interfaces/bss.rs
+++ crates/manta-backend-dispatcher/src/interfaces/bss.rs
@@ -3,10 +3,15 @@ use std::future::Future;
 use crate::{error::Error, types::BootParameters};
 
 pub trait BootParametersTrait {
+    fn get_all_bootparameters(
+        &self,
+        auth_token: &str,
+    ) -> impl Future<Output = Result<Vec<BootParameters>, Error>> + Send;
+
     fn get_bootparameters(
         &self,
         auth_token: &str,
-        nodes: &[String], // FIXME: This argument should be a struct of type BootParameters
+        nodes: &[String],
     ) -> impl Future<Output = Result<Vec<BootParameters>, Error>> + Send;
 
     fn add_bootparameters(

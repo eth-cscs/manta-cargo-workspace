06369c4a0e4c5082df96ae61a49c36dca147aecc
Alexandre Escoubas <alexandre.escoubas@cscs.ch>
2025-05-23 16:17:45 +0200

    feat: implement power-status
    - Implement /node/{node}/power-status on manta-ws

--- crates/manta-backend-dispatcher/src/interfaces/pcs.rs
+++ crates/manta-backend-dispatcher/src/interfaces/pcs.rs
@@ -1,16 +1,15 @@
 use serde_json::Value;
 
 use crate::error::Error;
+use crate::types::pcs::power_status::types::PowerStatusAll;
 
 pub trait PCSTrait {
-  // FIXME: Create a new type PowerStatus and return Result<PowerStatus, Error>
   fn power_on_sync(
     &self,
     _auth_token: &str,
     _nodes: &[String],
   ) -> impl std::future::Future<Output = Result<Value, Error>> + Send;
 
-  // FIXME: Create a new type PowerStatus and return Result<PowerStatus, Error>
   fn power_off_sync(
     &self,
     _auth_token: &str,
@@ -18,11 +17,20 @@ pub trait PCSTrait {
     _force: bool,
   ) -> impl std::future::Future<Output = Result<Value, Error>> + Send;
 
-  // FIXME: Create a new type PowerStatus and return Result<PowerStatus, Error>
   fn power_reset_sync(
     &self,
     _auth_token: &str,
     _nodes: &[String],
     _force: bool,
   ) -> impl std::future::Future<Output = Result<Value, Error>> + Send;
+
+  // FIXME: Create a new type PowerStatus and return Result<PowerStatus, Error>
+  fn power_status(
+    &self,
+    _auth_token: &str,
+    //_nodes: &[String],
+    _nodes: &[String],
+    _power_status_filter: Option<&str>,
+    _management_state_filter: Option<&str>,
+  ) -> impl std::future::Future<Output = Result<PowerStatusAll, Error>> + Send; // TODO: this should return PowerStatusAll instead
 }

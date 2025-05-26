84fd9763cb1dad776a50c2948af244f0cd54edbc
Manuel Sopena Ballesteros <masber@hotmail.com>
2025-05-26 02:48:30 +0200

    refactor: clean code

--- crates/csm-rs/src/pcs/power_status/http_client.rs
+++ crates/csm-rs/src/pcs/power_status/http_client.rs
@@ -1,13 +1,10 @@
-use serde_json::{ Value, json };
+use serde_json::{json, Value};
 
 use crate::error::Error;
 
-use super::types::{
-    PowerStatus,
-    PowerStatusAll
-};
+use super::types::PowerStatusAll;
 
-pub async fn get(
+pub async fn post(
   shasta_base_url: &str,
   shasta_token: &str,
   shasta_root_cert: &[u8],
@@ -34,92 +31,37 @@ pub async fn get(
 
   let api_url = format!("{}/power-control/v1/power-status", shasta_base_url);
 
-  let xname_vec_str_opt: Option<String> =
-    xname_vec_opt.map(|xname_vec| xname_vec.join(","));
-
-      let body = json!({
-        // TODO: this should probably be a struct that matches the expected schema
-        // here "xname" is an array of xnames
-        "xname": xname_vec_opt.map(|xname_vec| xname_vec.iter().map(|&x| x.to_string()).collect::<Vec<String>>()).unwrap_or_default(),
-        "powerStateFilter": power_state_filter_opt.unwrap_or(""),
-        "managementStateFilter": management_state_filter_opt.unwrap_or(""),
-    });
-
-    let response = client
-        .post(&api_url)
-        .json(&body) // Send the body as JSON
-        .bearer_auth(shasta_token)
-        .send()
-        .await
-        .map_err(|error| {
-            println!("Failed POST query: {:?}", error);
-            Error::NetError(error)
-        })?;
-
-  if response.status().is_success() {
-    println!("Response is success");
-    response
-      .json()
-      .await
-      .map_err(|error| {
-          println!("{:?}", error);
-          Error::NetError(error)
-      })
-  } else {
-    println!("Response is failure");
-    let payload = response
-      .json::<Value>()
-      .await
-      .map_err(|error| { 
-          println!("{:?}", error);
-          Error::NetError(error)
-      })?;
-
-    Err(Error::CsmError(payload))
-  }
-}
-
-pub async fn post(
-  shasta_base_url: &str,
-  shasta_token: &str,
-  shasta_root_cert: &[u8],
-  power_status: PowerStatus,
-) -> Result<PowerStatus, Error> {
-  log::info!("Create PCS power status:\n'{:#?}'", power_status);
-  log::debug!("Create PCS power status:\n{:#?}", power_status);
-
-  let client_builder = reqwest::Client::builder()
-    .add_root_certificate(reqwest::Certificate::from_pem(shasta_root_cert)?);
-
-  // Build client
-  let client = if let Ok(socks5_env) = std::env::var("SOCKS5") {
-    // socks5 proxy
-    log::debug!("SOCKS5 enabled");
-    let socks5proxy = reqwest::Proxy::all(socks5_env)?;
-
-    // rest client to authenticate
-    client_builder.proxy(socks5proxy).build()?
-  } else {
-    client_builder.build()?
-  };
-
-  let api_url = shasta_base_url.to_owned() + "/power-control/v1/power-status";
+  let body = json!({
+      // TODO: this should probably be a struct that matches the expected schema
+      // here "xname" is an array of xnames
+      "xname": xname_vec_opt.map(|xname_vec| xname_vec.iter().map(|&x| x.to_string()).collect::<Vec<String>>()).unwrap_or_default(),
+      "powerStateFilter": power_state_filter_opt.unwrap_or(""),
+      "managementStateFilter": management_state_filter_opt.unwrap_or(""),
+  });
 
   let response = client
-    .put(api_url)
-    .json(&power_status)
+    .post(&api_url)
+    .json(&body) // Send the body as JSON
     .bearer_auth(shasta_token)
     .send()
     .await
-    .map_err(|e| Error::NetError(e))?;
+    .map_err(|error| {
+      println!("Failed POST query: {:?}", error);
+      Error::NetError(error)
+    })?;
 
   if response.status().is_success() {
-    Ok(response.json().await.map_err(|e| Error::NetError(e))?)
+    println!("Response is success");
+    response.json().await.map_err(|error| {
+      println!("{:?}", error);
+      Error::NetError(error)
+    })
   } else {
-    let payload = response
-      .json::<Value>()
-      .await
-      .map_err(|e| Error::NetError(e))?;
+    println!("Response is failure");
+    let payload = response.json::<Value>().await.map_err(|error| {
+      println!("{:?}", error);
+      Error::NetError(error)
+    })?;
 
     Err(Error::CsmError(payload))
   }

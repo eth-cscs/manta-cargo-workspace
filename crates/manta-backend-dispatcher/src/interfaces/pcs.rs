f91dce07c37d1b8b670d24cd4c02598d0805ae6c
Manuel Sopena Ballesteros <masber@hotmail.com>
2025-06-01 19:35:46 +0200

    feat: manta cli code won't compile because it does not implements function get power status defined in PCSTrait. This patch fixes this by defining default behavious for PCSTrait functions so we don't need to define it in manta cli code

--- crates/manta-backend-dispatcher/src/interfaces/pcs.rs
+++ crates/manta-backend-dispatcher/src/interfaces/pcs.rs
@@ -8,21 +8,39 @@ pub trait PCSTrait {
     &self,
     _auth_token: &str,
     _nodes: &[String],
-  ) -> impl std::future::Future<Output = Result<Value, Error>> + Send;
+  ) -> impl std::future::Future<Output = Result<Value, Error>> + Send {
+    async {
+      Err(Error::Message(
+        "Power on command not implemented for this backend".to_string(),
+      ))
+    }
+  }
 
   fn power_off_sync(
     &self,
     _auth_token: &str,
     _nodes: &[String],
     _force: bool,
-  ) -> impl std::future::Future<Output = Result<Value, Error>> + Send;
+  ) -> impl std::future::Future<Output = Result<Value, Error>> + Send {
+    async {
+      Err(Error::Message(
+        "Power off command not implemented for this backend".to_string(),
+      ))
+    }
+  }
 
   fn power_reset_sync(
     &self,
     _auth_token: &str,
     _nodes: &[String],
     _force: bool,
-  ) -> impl std::future::Future<Output = Result<Value, Error>> + Send;
+  ) -> impl std::future::Future<Output = Result<Value, Error>> + Send {
+    async {
+      Err(Error::Message(
+        "Power reset command not implemented for this backend".to_string(),
+      ))
+    }
+  }
 
   // FIXME: Create a new type PowerStatus and return Result<PowerStatus, Error>
   fn power_status(
@@ -32,5 +50,13 @@ pub trait PCSTrait {
     _nodes: &[String],
     _power_status_filter: Option<&str>,
     _management_state_filter: Option<&str>,
-  ) -> impl std::future::Future<Output = Result<PowerStatusAll, Error>> + Send; // TODO: this should return PowerStatusAll instead
+  ) -> impl std::future::Future<Output = Result<PowerStatusAll, Error>> + Send
+  {
+    // TODO: this should return PowerStatusAll instead
+    async {
+      Err(Error::Message(
+        "Power status command not implemented for this backend".to_string(),
+      ))
+    }
+  }
 }
